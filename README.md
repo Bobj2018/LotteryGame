a3c250e78f2bd5d1ca378e93f5739d3b045804b7
Joshua Rieth <joshua.m.rieth@gmail.com>
2018-01-02 18:28:07 -0500

    add html and css files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-DAY 1 - JAN. 01 18
+DAY 2 - JAN. 2ND 2018
+- Implemented the HTML
+- Added Bootstrap
+
+DAY 1 - JAN. 1ST 2018
 - Initial Release
--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 
-//Creates Random Number
-const createRandomNumber = () => {
-	//Create an array to store numbers
-	let randomNumbers = []
-	//Create numbers 6 times
-	for (let i = 0; i < 6; i++) {
-		//Create number between 1 - 50
-		randomNumbers.push(Math.floor(Math.random() * 51));
-	}
-	//return array
+// //Creates Random Number
+// const createRandomNumber = () => {
+// 	//Create an array to store numbers
+// 	let randomNumbers = []
+// 	//Create numbers 6 times
+// 	for (let i = 0; i < 6; i++) {
+// 		//Create number between 1 - 50
+// 		randomNumbers.push(Math.floor(Math.random() * 51));
+// 	}
+// 	//return array
 	
-	return randomNumbers;
-}
+// 	return randomNumbers;
+// }
 
-const isGuessCorrect = (userGuesses, randomNumbers) => {
-	let correctGuesses = []
-	for (let i = 0; i < 6; i++) {
-		if (Number(userGuesses[i]) === randomNumbers[i]){
-			correctGuesses.push(true);
-		} else {
-			correctGuesses.push(false);
-		}
-	}
-	return correctGuesses;
-}
+// const isGuessCorrect = (userGuesses, randomNumbers) => {
+// 	let correctGuesses = []
+// 	for (let i = 0; i < 6; i++) {
+// 		if (Number(userGuesses[i]) === randomNumbers[i]){
+// 			correctGuesses.push(true);
+// 		} else {
+// 			correctGuesses.push(false);
+// 		}
+// 	}
+// 	return correctGuesses;
+// }
 
-const playGame = () => {
-	let randomNumbers = createRandomNumber();
-	console.log(randomNumbers)
-	let userGuesses = prompt("Input 6 numbers (seperate with a comma)");
-	userGuesses = userGuesses.split(",");
-	console.log(userGuesses)
+// const playGame = () => {
+// 	let randomNumbers = createRandomNumber();
+// 	console.log(randomNumbers)
+// 	let userGuesses = prompt("Input 6 numbers (seperate with a comma)");
+// 	userGuesses = userGuesses.split(",");
+// 	console.log(userGuesses)
 
 
-	return isGuessCorrect(userGuesses, randomNumbers);
-}
+// 	return isGuessCorrect(userGuesses, randomNumbers);
+// }
 
-console.log(playGame());
+// console.log(playGame());
