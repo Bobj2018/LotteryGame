e0bc8a0fa77c11d6b0d297326025ec54a64742b3
Joshua Rieth <joshua.m.rieth@gmail.com>
2018-01-03 20:57:12 -0500

    made the html work with JS

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+DAY 3 - JAN. 3RD 2018
+- Added a submit button
+- Removed prompt box & .split 
+- Made the Form hode when submitted
+
 DAY 2 - JAN. 2ND 2018
 - Implemented the HTML
 - Added Bootstrap
--- js/app.js
+++ js/app.js
@@ -1,39 +1,55 @@
+let numberForm = document.getElementById("numberForm")
+let subBtn = document.getElementById("subBtn");
+let formDiv = document.getElementById("formDiv");
+let userDiv = document.getElementById("userDiv");
+let randomDiv = document.getElementById("randomDiv");
+console.log(numberForm);
 
-// //Creates Random Number
-// const createRandomNumber = () => {
-// 	//Create an array to store numbers
-// 	let randomNumbers = []
-// 	//Create numbers 6 times
-// 	for (let i = 0; i < 6; i++) {
-// 		//Create number between 1 - 50
-// 		randomNumbers.push(Math.floor(Math.random() * 51));
-// 	}
-// 	//return array
+let userGuesses = []
+
+subBtn.onclick = function() {
+for (let i = 0; i < 6; i++){
+	userGuesses.push(Number(numberForm.elements[i].value));
+}
+formDiv.classList.toggle("hideDIV");
+userDiv.classList.toggle("hideDIV");
+randomDiv.classList.toggle("hideDIV");
+};
+
+//Creates Random Number
+const createRandomNumber = () => {
+	//Create an array to store numbers
+	let randomNumbers = []
+	//Create numbers 6 times
+	for (let i = 0; i < 6; i++) {
+		//Create number between 1 - 50
+		randomNumbers.push(Math.floor(Math.random() * 51));
+	}
+	//return array
 	
-// 	return randomNumbers;
-// }
+	return randomNumbers;
+}
 
-// const isGuessCorrect = (userGuesses, randomNumbers) => {
-// 	let correctGuesses = []
-// 	for (let i = 0; i < 6; i++) {
-// 		if (Number(userGuesses[i]) === randomNumbers[i]){
-// 			correctGuesses.push(true);
-// 		} else {
-// 			correctGuesses.push(false);
-// 		}
-// 	}
-// 	return correctGuesses;
-// }
+const isGuessCorrect = (userGuesses, randomNumbers) => {
+	let correctGuesses = []
+	for (let i = 0; i < 6; i++) {
+		if (Number(userGuesses[i]) === randomNumbers[i]){
+			correctGuesses.push(true);
+		} else {
+			correctGuesses.push(false);
+		}
+	}
+	return correctGuesses;
+}
 
-// const playGame = () => {
-// 	let randomNumbers = createRandomNumber();
-// 	console.log(randomNumbers)
-// 	let userGuesses = prompt("Input 6 numbers (seperate with a comma)");
-// 	userGuesses = userGuesses.split(",");
-// 	console.log(userGuesses)
+const playGame = () => {
+	let randomNumbers = createRandomNumber();
+	console.log(randomNumbers)
+	console.log(userGuesses);
 
 
-// 	return isGuessCorrect(userGuesses, randomNumbers);
-// }
+	return isGuessCorrect(userGuesses, randomNumbers);
+}
 
-// console.log(playGame());
+//console.log(playGame());
+//
